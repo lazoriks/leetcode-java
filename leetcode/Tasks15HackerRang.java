5d0de2e055261f4993ddf78d0a986642bbf34862
Stepan <31982882+lazoriks@users.noreply.github.com>
2026-01-20 12:58:56 +0000

    oddNumbers

--- leetcode/Tasks15HackerRang.java
+++ leetcode/Tasks15HackerRang.java
@@ -10,6 +10,18 @@ public static String findNumber(List<Integer> arr, int k) {
         return "NO";
     }
 
+public static List<Integer> oddNumbers(int l, int r) {
+        List<Integer> oddNumbersList = new ArrayList<>();
+        for (int i = l; i <= r; i++) {
+            if (i % 2 != 0) {
+                oddNumbersList.add(i);
+            }
+        }
+        return oddNumbersList;
+        
+    }
+    
+
 public static void main(String[] args) {
         test solution = new test();
 
