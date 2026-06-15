b71de76944ced6d379c78dbe4bdadcc80600ceb5
Stepan <31982882+lazoriks@users.noreply.github.com>
2026-06-15 15:45:54 +0100

    optimized reverse

--- leetcode/FindDuplicates.java
+++ leetcode/FindDuplicates.java
@@ -38,11 +38,17 @@ public class FindDuplicates {
         char[] chars = s.toCharArray();
         int left = 0, right = chars.length - 1;
         while (left < right) {
-            char temp = chars[left];
-            chars[left] = chars[right];
-            chars[right] = temp;
-            left++;
-            right--;
+            if (chars[left] == ' ')
+                left++; // пропускаємо пробіл зліва
+            else if (chars[right] == ' ')
+                right--; // пропускаємо пробіл справа
+            else {
+                char temp = chars[left];
+                chars[left] = chars[right];
+                chars[right] = temp;
+                left++;
+                right--;
+            }
         }
         return new String(chars);
     }
